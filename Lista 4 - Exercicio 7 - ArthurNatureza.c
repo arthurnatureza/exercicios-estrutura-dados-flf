665b32ac49111dac8ef4c22ac4a1a56a5bd3ea82
Arthur Natureza <arthurnatureza@gmail.com>
2020-09-10 20:30:14 -0300

    Update Lista 4 - Exercicio 7 - ArthurNatureza.c
    Dois exemplos a mais

--- Lista 4 - Exercicio 7 - ArthurNatureza.c
+++ Lista 4 - Exercicio 7 - ArthurNatureza.c
@@ -25,3 +25,33 @@ int main(){
     printf("%d hora(s), %d minuto(s), %d segundo(s) equivale a %d segundos", hora, minuto, segundo, resp);
 
 }
+
+
+/*#include <stdio.h>
+
+int segundos(int hr, int min, int sg){
+    return (hr*3600 + min*60 + sg);
+}
+int main(){
+   int hr, min, sg;
+   printf("Informe uma hora (hora:min:seg): ");
+   scanf("%d:%d:%d", &hr, &min, &sg);
+   printf("A hora em segundos é = %d",  segundos(hr,min,sg));
+}*/
+
+/*#include <stdio.h>
+
+int calculateSeconds(int hours, int minutes, int seconds) {
+    int totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+
+    return totalSeconds;
+}
+
+
+void main() {
+    int hours=0, minutes=0, seconds=0;
+
+    printf("Informe horas, minutos e segundos: ");
+    scanf("%d %d %d", &hours, &minutes, &seconds);
+    printf("Essa hora vale %d segundos.\n", calculateSeconds(hours, minutes, seconds));
+}*/
